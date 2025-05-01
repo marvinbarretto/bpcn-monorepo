35fc829cb038c818dfc8c8d8c2cb2411ebcb9a42
Marvin <marvinbarretto@gmail.com>
2025-05-01 12:20:41 +0100

    delete backend

--- apps/backend/config/plugins.ts
+++
@@ -1,8 +0,0 @@
-export default ({ env }) => ({
-    'users-permissions': {
-      config: {
-        jwtSecret: env('JWT_SECRET'),
-      },
-    },
-  });
-  
--- apps/backend/src/index.ts
+++
@@ -1,6 +0,0 @@
-import strapi from '@strapi/core';
-
-export default strapi({
-  register() {},
-  bootstrap() {},
-});
