727a0c836f9e3e896dc3cf713722695c124ceb26
Marvin <marvinbarretto@gmail.com>
2025-05-01 12:39:11 +0100

    Seed users

--- apps/strapi/src/index.ts
+++ apps/strapi/src/index.ts
@@ -16,5 +16,44 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  async bootstrap({ strapi }) {
+    const anyExistingUsers = await strapi.query('plugin::users-permissions.user').findMany();
+    if (anyExistingUsers.length > 0) return;
+
+    const roles = await strapi.query('plugin::users-permissions.role').findMany();
+    const roleMap = Object.fromEntries(roles.map(role => [role.name, role]));
+
+    const usersToCreate = [
+      {
+        username: 'public',
+        email: 'public@test.com',
+        password: '123',
+        role: roleMap['Public'],
+      },
+      {
+        username: 'auth',
+        email: 'auth@test.com',
+        password: '123',
+        role: roleMap['Authenticated'],
+      },
+      {
+        username: 'author',
+        email: 'author@test.com',
+        password: '123',
+        role: roleMap['Author'],
+      },
+      {
+        username: 'admin',
+        email: 'admin@test.com',
+        password: '123',
+        role: roleMap['Admin'],
+      },
+    ];
+
+    for (const user of usersToCreate) {
+      await strapi.query('plugin::users-permissions.user').create({ data: user });
+    }
+
+    console.log('*** Users seeded');
+  },
 };
